3ab11874e373a0ea8f6158b2e6b9ecaa75b5f257
parkmansoo <parkman.xxx@gmail.com>
2023-10-22 19:18:08 +0900

    #43 - 해시태그 검색 페이지를 위한 기능 구현
    해시태그 뷰에 모든 게시글에서 나오는 해시태그를
    정리해서 보여주는 기능을 넣기 위해,
    해시태그만 뽑아서 unique 하게 보여주는 쿼리를
    querydsl 로 작성함
    그 외 서비스 코드, 컨트롤러, 관련 테스트 작성

--- src/main/java/com/project/board/controller/ArticleController.java
+++ src/main/java/com/project/board/controller/ArticleController.java
@@ -55,4 +55,24 @@ public class ArticleController {
         return "articles/detail";
     }
 
+    @GetMapping("/search-hashtag")
+    public String searchHashtag (
+            @RequestParam(required = false) String searchValue,
+            @PageableDefault(size = 10,sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable,
+            ModelMap map
+    ) {
+        Page<ArticleResponse> articles =  articleService.searchArticlesViaHashtag(searchValue,pageable).map(ArticleResponse::from);
+        List<Integer> barNumbers = paginationService.getPaginationBarNumbers(pageable.getPageNumber(), articles.getTotalPages());
+        List<String> hashtag = articleService.getHashtags();
+
+        map.addAttribute("articles", articles);
+        map.addAttribute("hashtags", hashtag);
+        map.addAttribute("paginationBarNumbers", barNumbers);
+        map.addAttribute("searchType", SearchType.HASHTAG);
+
+        return "articles/search-hashtag";
+
+    }
+
+
 }
