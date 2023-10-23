60a77e5269f0a8b77844c3f0629e6c6659f0b720
parkmansoo <parkman.xxx@gmail.com>
2023-10-23 23:22:17 +0900

    #49 - 리팩토링: UserAccountDto 에 메타 정보를 넣지 않는 of() 팩토리 메소드 추가
    테스트를 용이하게 하기 위해 추가함
    
    테스트 목적 외에도,
    repository 레이어에 영속되지 않은 객체는
    메타 정보가 없는 것이 당연하므로
    새로운 데이터를 만들 때 이 부분을 입력하지 않아도 되는
    팩토리 메소드를 만들어주는 것이 의미가 있음

--- src/main/java/com/project/board/controller/ArticleController.java
+++ src/main/java/com/project/board/controller/ArticleController.java
@@ -3,6 +3,7 @@ package com.project.board.controller;
 import com.project.board.domain.constant.FormStatus;
 import com.project.board.domain.constant.SearchType;
 import com.project.board.dto.UserAccountDto;
+import com.project.board.dto.request.ArticleRequest;
 import com.project.board.dto.response.ArticleResponse;
 import com.project.board.dto.response.ArticleWithCommentsResponse;
 import com.project.board.service.ArticleService;
@@ -83,7 +84,7 @@ public class ArticleController {
     public String postNewArticle(ArticleRequest articleRequest) {
         // TODO: 인증 정보를 넣어줘야 한다.
         articleService.saveArticle(articleRequest.toDto(UserAccountDto.of(
-                "uno", "asdf1234", "uno@mail.com", "Uno", "memo", null, null, null, null
+                "uno", "asdf1234", "uno@mail.com", "Uno", "memo"
         )));
 
         return "redirect:/articles";
@@ -103,7 +104,7 @@ public class ArticleController {
     public String updateArticle(@PathVariable Long articleId, ArticleRequest articleRequest) {
         // TODO: 인증 정보를 넣어줘야 한다.
         articleService.updateArticle(articleId, articleRequest.toDto(UserAccountDto.of(
-                "uno", "asdf1234", "uno@mail.com", "Uno", "memo", null, null, null, null
+                "uno", "asdf1234", "uno@mail.com", "Uno", "memo"
         )));
 
         return "redirect:/articles/" + articleId;
